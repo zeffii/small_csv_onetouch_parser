d19ba02ca6cf473790d789a4c8daa5e07e98ab46
zeffii <tetha.z@gmail.com>
2017-08-13 20:47:36 +0200

    complete day generator

--- glucose/data_script.py
+++ glucose/data_script.py
@@ -1,5 +1,6 @@
 import datetime
 
+# week 32 | Aug 7 - Aug 13
 template_head = """\
 
 --------
@@ -9,18 +10,20 @@ template_head = """\
 <details>
 """
 
-# week 32 | Aug 7 - Aug 13
+# ##### 2017, Aug 7 --- Mon
 template_body = """\
 
 
-##### {YYYY}, {DAY_MM_D} --- {DAYNAME}
+##### {YYYY}, {MM_D_NAME}
 
 |Time | Glucose | Units | Comment|
-|-------|------|--------|---------------------------------|
+|-------|------|--------|---------------------------------|"""
 
-"""
+template_day_graph_lines = """\
+|       |      |        |                                 |"""
 
 template_tail = """\
+
 </details>"""
 
 
@@ -46,6 +49,15 @@ def compose_chart(week_nr, year, num_chart_entries=5):
     sd = sd.date().strftime("%b %d")
     ed = ed.date().strftime("%b %d")
     print(template_head.format(WEEK=week_nr, START_DAY=sd, END_DAY=ed))
+
+    for day in days:
+        tstr = template_body.format(YYYY=str(year) , MM_D_NAME=day.date().strftime("%b %d --- %a"))
+        print(tstr)
+        for i in range(num_chart_entries):
+            print(template_day_graph_lines)
+
+    print(template_tail)
+
 
 
 compose_chart(week_nr=31, year=2017, num_chart_entries=5)
